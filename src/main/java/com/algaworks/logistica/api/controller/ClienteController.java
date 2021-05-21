fb7e6bb65f6a94d206070e1742ec376098aa9408
Allmeida <allmeida.fran@gmail.com>
2021-05-21 02:24:19 -0400

    API logistica - conexão com o banco

--- src/main/java/com/algaworks/logistica/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/logistica/api/controller/ClienteController.java
@@ -15,7 +15,7 @@ public class ClienteController {
 	public List<Cliente> listar() {
 		var cliente1 = new Cliente();
 		cliente1.setId(1L);
-		cliente1.setNome("João");
+		cliente1.setNome("João3");
 		cliente1.setTelefone("34 99999-1111");
 		cliente1.setEmail("joaodascouves@algaworks.com");
 		
