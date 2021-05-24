ed94f5ed0c852b4e8f302477cf4274fec8614b06
Allmeida <allmeida.fran@gmail.com>
2021-05-24 01:11:35 -0400

    API logistica - Listando clientes e retornando codigo de status 200 ou 404

--- src/main/java/com/algaworks/logistica/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/logistica/api/controller/ClienteController.java
@@ -1,9 +1,13 @@
 package com.algaworks.logistica.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.logistica.domain.model.Cliente;
@@ -13,14 +17,21 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
-		
-	@Autowired
+	
 	private ClienteRepository clienteRepository;
 
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
-		return clienteRepository.findAll();
+		return clienteRepository.findAll(); // retorna todos clientes
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)		    // retorna apenas um cliente
+					.map(ResponseEntity::ok)				    // verifica se existe o cliente e retorna um status 200
+					.orElse(ResponseEntity.notFound().build()); // se o cliente nao existir, retorna um status 404
 	}
 	
 }
